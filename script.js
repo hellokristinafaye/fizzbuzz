889dd6dd3754b02cd846c77bd6ff09b1534ca8c6
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-02 15:16:33 -0700

    condition for divisible by 3 aka Fizz

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let answer = parseInt(prompt("Please enter the number to which you want FizzBuzz to count to:"));
 
 for (let i = 1; i <= answer; i++) {
-    
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    } else {
+        console.log(i);
+    }
 }
