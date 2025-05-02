33481472648f3d38440821916bf4d556ee429382
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-02 15:20:15 -0700

    final condition added for divisible by 3 AND 5

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let answer = parseInt(prompt("Please enter the number to which you want FizzBuzz to count to:"));
 
 for (let i = 1; i <= answer; i++) {
-    if (i % 3 === 0) {
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
         console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
     } else {
         console.log(i);
     }
